c581f097fb3598c6827668966f292e86a2790ef3
yousufzai8454 <yousufzai8454@gmail.com>
2022-06-21 21:43:30 -0400

    This is my Second Cimmit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	for (int i=0; i<5; i++){
         System.out.println("Hello! I am a human");
+        System.out.println("Hello! My name is Salim");
     }
     }
 }
