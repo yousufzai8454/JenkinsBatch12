4e76289245f53b7ea5add8429e12d954fd40101a
yousufzai8454 <yousufzai8454@gmail.com>
2022-06-21 21:08:04 -0400

    My first Jenkins project

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,10 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+	for (int i=0; i<5; i++){
+        System.out.println("Hello! I am a human");
+    }
+    }
+}
